70e05c48714589a88cc42e7d67f6dbde64bb624c
Roneison Machado <roneison.machado@gmail.com>
2020-02-01 17:25:22 -0300

    268. Controlando o formulário de login

--- src/app/acesso/login/login.component.ts
+++ src/app/acesso/login/login.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormControl } from '@angular/forms';
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
@@ -9,6 +10,11 @@ export class LoginComponent implements OnInit {
 
   @Output() public exibirPainel: EventEmitter<string> = new EventEmitter();
 
+  public formulario: FormGroup = new FormGroup({
+    'email': new FormControl(null),
+    'senha': new FormControl(null)
+  });
+
   constructor() { }
 
   ngOnInit() {
@@ -16,6 +22,10 @@ export class LoginComponent implements OnInit {
 
   public exibirPainelCadastro(): void {
     this.exibirPainel.emit('cadastro');
+  }
+
+  public autenticar(): void {
+    console.log(this.formulario);
   }
 
 }
